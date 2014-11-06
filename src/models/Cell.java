4a7efcac872b14aaf277d314ad42b39ab4a06429
Kommendant <jonasd90@gmail.com>
2014-11-06 16:31:59 +0100

    Changes to cell

--- src/models/Cell.java
+++ src/models/Cell.java
@@ -2,6 +2,11 @@ package models;
 
 import expr.Expr;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Cell {
 	public Sheet sheet;
 	public String address;
@@ -10,6 +15,22 @@ public class Cell {
 	public Cell(Sheet sheet, String address) {
 		this.sheet = sheet;
 		this.address = address;
+	}
+
+	public void listenToVars(Sheet sheet) {
+		variables();
+	}
+
+	public List<String> variables() {
+		List<String> vars = new ArrayList<String>();
+
+		Pattern p = Pattern.compile("[A-Z]+[1-9][0-9]*");
+		Matcher m = p.matcher(expr.toString());
+		while(m.find()) {
+			vars.add(m.group());
+		}
+
+		return vars;
 	}
 	
 	public void change() {
