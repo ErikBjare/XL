ddad2d4b8e0702fab06eae0b0216db26b20d1795
Kommendant <jonasd90@gmail.com>
2014-11-17 22:55:47 +0100

    Changed so that leaving a "#" after editing a slot and pressing enter now correctly makes the slot an empty slot.

--- src/gui/Editor.java
+++ src/gui/Editor.java
@@ -2,6 +2,10 @@ package gui;
 
 import expr.Expr;
 import expr.ExprParser;
+import models.CommentSlot;
+import models.ExprSlot;
+import models.Sheet;
+import models.Slot;
 
 import java.awt.Color;
 import java.awt.event.ActionEvent;
@@ -12,34 +16,60 @@ import java.util.Observer;
 import javax.swing.JTextField;
 
 public class Editor extends JTextField implements Observer {
-    public Editor() {
+    private CurrentSlot currentSlot;
+    private Sheet sheet;
+
+    public Editor(final Sheet sheet, final CurrentSlot currentSlot) {
+        this.currentSlot = currentSlot;
+        this.sheet = sheet;
+        currentSlot.addObserver(this);
+
         setBackground(Color.WHITE);
-        ActionListener enterAction = new ActionListener() {
+
+        addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                if(getText().charAt(0) == '#') {
-                    // If Comment
-                    // TODO: Create a commentslot
-                    System.out.println("Commentslot to-be");
-                } else {
+                Slot slot;
+                String text = getText();
+                if (text.length() > 0 && text.charAt(0) != '#') {
                     // If Expression
                     // TODO: Create a exprslot
-                    System.out.println("KLINGONS EVERYWHERE, they are forcing you to evaluate the expression: " + getText());
+
                     try {
-                        Expr expr = new ExprParser().build(getText());
-                        System.out.println("You succeeded and they gave you 10 bars of gold-pressed latinum: " + expr);
+                        Expr expr = new ExprParser().build(text);
+                        slot = new ExprSlot(sheet, currentSlot.getAddress(), expr);
                     } catch (IOException err) {
-                        System.out.println("Klingons killed you for failing to evaluate the expression.");
+                        // TODO: Do something with error
+                        System.err.println("IOERROR");
+                        return;
                     }
+                    sheet.put(currentSlot.getAddress(), slot);
+
+                    } else if (text.charAt(0) == '#' && text.length() > 1){
+                    // If Comment
+                    slot = new CommentSlot(sheet, currentSlot.getAddress(), text.substring(1, text.length()));
+                    System.out.println(text.substring(1, text.length()));
+                    sheet.put(currentSlot.getAddress(), slot);
+                    } else {
+
+                    sheet.clear(currentSlot.getAddress());
+
+
                 }
             }
-        };
-        addActionListener(enterAction);
+        });
     }
 
     @Override
     public void update(Observable observable, Object o) {
-        // TODO: Set expression to slot and read input of current slot
+        Slot slot = sheet.get(currentSlot.getAddress());
+        if (slot != null) {
 
+                setText(slot.getText());
+
+        } else {
+            System.out.println("Empty Slot ");
+            setText("");
+        }
     }
 }
